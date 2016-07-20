30c5f3b49d39afc9c6d48252f8d427b95a2e9a9b
karan1149 <karan1149@gmail.com>
2016-07-20 12:14:59 -0400

    Initial attempt to get daily returns

---
+++ simulator.py
@@ -0,0 +1,30 @@
+# Using QuantSoftwareToolKit and concommitant libraries
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def simulate(ls_symbols, weights):
+    dt_timeofday = dt.timedelta(hours=16)
+    dt_start = dt.datetime(2006, 1, 1)
+    dt_end = dt.datetime(2010, 12, 31)
+    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+
+    c_dataobj = da.DataAccess('Yahoo')
+    ls_keys = ['close']
+    key_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+    data = dict(zip(ls_keys, key_data))
+    prices = np.array(data.values()[0])
+
+    prices_norm = prices / prices[0, : ]
+    weighted_prices = 0
+
+    // assume number of symbols == number of weights
+    for i in range(len(weights)):
+
+    daily_returns = prices_norm.copy()
+    tsu.returnize0(daily_returns)
+    return daily_returns
